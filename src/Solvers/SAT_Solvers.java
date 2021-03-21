237ce7d1ac56dffae280aa87bd9eb25061fe8782
ClayF143 <48032691+ClayF143@users.noreply.github.com>
2021-03-21 03:05:52 -0400

    initial commit

--- src/Solvers/SAT_Solvers.java
+++ src/Solvers/SAT_Solvers.java
@@ -51,6 +51,24 @@ public class SAT_Solvers
 		}
 	}
 	
+	// returns -1 if the clause is false,
+	// 0 if it is unknown, and 1 if true
+	private int clauseTF(int [] clause, HashMap<Integer, Boolean> model)
+	{
+		boolean unknown = false;
+		for(int var : clause)
+		{
+			Boolean b = model.get(Math.abs(var));
+			if(b == null)
+				unknown = true;
+			else if((b.booleanValue() && var < 0) || !b.booleanValue() && var > 0)
+				return -1;
+		}
+		if(unknown)
+			return 0;
+		return 1;
+	}
+	
 	public boolean DPLL(ArrayList<int []> clauses, ArrayList<Integer> symbols, HashMap<Integer, Boolean> model)
 	{
 		// return true if all clauses are true and return false if any clause is false
@@ -74,50 +92,56 @@ public class SAT_Solvers
 		if(pureSymbol != 0)
 		{
 			if(pureSymbol > 0)
-			{
-				pureSymbol = Math.abs(pureSymbol);
-				model.put(pureSymbol, Boolean.valueOf(true));
-				symbols.remove(symbols.indexOf(pureSymbol));
-				return DPLL(clauses, symbols, model);
-			}
+				model.put(Math.abs(pureSymbol), Boolean.valueOf(true));
 			else
-			{
-				model.replace(pureSymbol, Integer.valueOf(0));
-				// more here
-				return DPLL();
-			}
+				model.put(Math.abs(pureSymbol), Boolean.valueOf(false));
+			
+			pureSymbol = Math.abs(pureSymbol);
+			symbols.remove(symbols.indexOf(pureSymbol));
+			return DPLL(clauses, symbols, model);
+		}
+		
+		int unitClauseSymbol = findUnitClause(clauses, symbols, model);
+		if(unitClauseSymbol != 0)
+		{
+			if(unitClauseSymbol > 0)
+				model.put(Math.abs(unitClauseSymbol), Boolean.valueOf(true));
+			else
+				model.put(Math.abs(unitClauseSymbol), Boolean.valueOf(false));
+			
+			unitClauseSymbol = Math.abs(unitClauseSymbol);
+			symbols.remove(symbols.indexOf(unitClauseSymbol));
+			return DPLL(clauses, symbols, model);
 		}
 		
 		// if you're out of those then actually do the dfs
-		
-		
-	}
-	
-	// returns -1 if the clause is false,
-	// 0 if it is unknown, and 1 if true
-	private int clauseTF(int [] clause, HashMap<Integer, Boolean> model)
-	{
-		boolean unknown = false;
-		for(int var : clause)
-		{
-			Boolean b = model.get(Math.abs(var));
-			if(b == null)
-				unknown = true;
-			else if((b.booleanValue() && var < 0) || !b.booleanValue() && var > 0)
-				return -1;
-		}
-		if(unknown)
-			return 0;
-		return 1;
 	}
 	
 	private int findPureSymbol(ArrayList<int []> clauses, ArrayList<Integer> symbols, HashMap<Integer, Boolean> model)
 	{
-		
+		return 0;
 	}
 	
-	private int findUnitClauses(ArrayList<int []> clauses, ArrayList<Integer> symbols, HashMap<Integer, Boolean> model)
+	private int findUnitClause(ArrayList<int []> clauses, ArrayList<Integer> symbols, HashMap<Integer, Boolean> model)
 	{
+		return 0;
+	}
+	
+	
+	public HashMap<Integer, Boolean> walkSAT(ArrayList<int []> clauses, long p, int maxFlips)
+	{
+		// randomize every symbol in model
 		
+		for(int i = 0; i < maxFlips; i++)
+		{
+			// if model is good then stop
+			int [] clause; // = random false clause in model
+			
+			// based on p do one of the following
+				// flip a random symbol in clause
+				// or flip the symbol that maximizes satisfied clauses in model after that flip
+		}
+		
+		return null;
 	}
 }
