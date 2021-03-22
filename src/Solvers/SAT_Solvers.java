48dc108261147ba18238223f9e045f51018612a2
ClayF143 <48032691+ClayF143@users.noreply.github.com>
2021-03-22 10:01:54 -0400

    finished untested dpll

--- src/Solvers/SAT_Solvers.java
+++ src/Solvers/SAT_Solvers.java
@@ -4,15 +4,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class SAT_Solvers
 {
-	private ArrayList<int []> clauses;
-	
-	public SAT_Solvers(String inputFilePath)
+	public ArrayList<int []> readClausesFromFile(String inputFilePath)
 	{
-		clauses = new ArrayList<int []>();
+		ArrayList<int []> clauses = new ArrayList<int []>();
 		try
 		{
 			File myFile = new File(inputFilePath);
@@ -26,6 +25,7 @@ public class SAT_Solvers
 			String data = "";
 			while (myReader.hasNextLine())
 				data += myReader.nextLine();
+			myReader.close();
 			
 			String [] clausesData = data.split("0");
 			for(String clause : clausesData)
@@ -49,6 +49,7 @@ public class SAT_Solvers
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
+		return clauses;
 	}
 	
 	// returns -1 if the clause is false,
@@ -61,8 +62,27 @@ public class SAT_Solvers
 			Boolean b = model.get(Math.abs(var));
 			if(b == null)
 				unknown = true;
-			else if((b.booleanValue() && var < 0) || !b.booleanValue() && var > 0)
+			else if((b.booleanValue() && var > 0) || !b.booleanValue() && var < 0)
+				return 1;
+		}
+		if(unknown)
+			return 0;
+		return -1;
+	}
+	
+	private int modelTF(ArrayList<int []> clauses, HashMap<Integer, Boolean> model)
+	{
+		boolean unknown = false;
+		for(int [] clause : clauses)
+		{
+			if(clauseTF(clause, model) == -1)
+			{
 				return -1;
+			}
+			if(clauseTF(clause, model) == 0)
+			{
+				unknown = true;
+			}
 		}
 		if(unknown)
 			return 0;
@@ -72,22 +92,13 @@ public class SAT_Solvers
 	public boolean DPLL(ArrayList<int []> clauses, ArrayList<Integer> symbols, HashMap<Integer, Boolean> model)
 	{
 		// return true if all clauses are true and return false if any clause is false
-		boolean allTrue = true;
-		for(int [] clause : clauses)
-		{
-			if(clauseTF(clause, model) != 1)
-			{
-				allTrue = false;
-			}
-			if(clauseTF(clause, model) == -1)
-			{
-				return false;
-			}
-		}
-		if(allTrue)
+		int isModSat = modelTF(clauses, model);
+		if(isModSat == 1)
 			return true;
+		if(isModSat == -1)
+			return false;
 		
-		// otherwise find pure symbols and unit clauses
+		// if unknown, find pure symbols and unit clauses
 		int pureSymbol = findPureSymbol(clauses, symbols, model);
 		if(pureSymbol != 0)
 		{
@@ -115,31 +126,116 @@ public class SAT_Solvers
 		}
 		
 		// if you're out of those then actually do the dfs
+		int first = symbols.get(0);
+		symbols.remove(0);
+		HashMap<Integer, Boolean> copy = (HashMap<Integer, Boolean>) model.clone();
+		model.put(Integer.valueOf(first), Boolean.valueOf(true));
+		copy.put(Integer.valueOf(first), Boolean.valueOf(false));
+		return DPLL(clauses, symbols, model) || DPLL(clauses, symbols, copy);
 	}
 	
 	private int findPureSymbol(ArrayList<int []> clauses, ArrayList<Integer> symbols, HashMap<Integer, Boolean> model)
 	{
+		// first, create a map of every symbol and whether it is positive or negative
+		// if ever you see a symbol that's already in the map and not consistent, give it a value of 0
+		HashMap<Integer, Integer> posNeg = new HashMap<Integer, Integer>();
+		for(int [] clause : clauses)
+		{
+			for(int sym : clause)
+			{
+				if(!posNeg.containsKey(sym))
+				{
+					if(sym > 0)
+						posNeg.put(Integer.valueOf(Math.abs(sym)), Integer.valueOf(1));
+					else
+						posNeg.put(Integer.valueOf(Math.abs(sym)), Integer.valueOf(-1));
+				}
+				else if((posNeg.get(Math.abs(sym)).equals(Integer.valueOf(1)) && sym < 0) 
+					|| (posNeg.get(Math.abs(sym)).equals(Integer.valueOf(-1)) && sym > 0))
+						posNeg.replace(Math.abs(sym), Integer.valueOf(0));
+			}
+		}
+		
+		// return any symbol that doesn't have a value of 0
+		for(Entry<Integer, Integer> entry : posNeg.entrySet())
+			if(! entry.getValue().equals(Integer.valueOf(0)))
+				return entry.getKey();
+		
+		// if there are none, return failure
 		return 0;
 	}
 	
 	private int findUnitClause(ArrayList<int []> clauses, ArrayList<Integer> symbols, HashMap<Integer, Boolean> model)
 	{
+		// a unit clause exists if every symbol in a clause is the same, including positivity and negativity
+		// if one is found, returns the symbol in that clause
+		for(int [] clause : clauses)
+		{
+			boolean b = true;
+			int sym = clause[0];
+			for(int s : clause)
+			{
+				if(s != sym)
+					b = false;
+			}
+			if(b)
+				return sym;
+		}
 		return 0;
+	}
+	
+	private int findRandFalseClauseIndex(ArrayList<int []> clauses, HashMap<Integer, Boolean> model)
+	{
+		// returns the index of a random false clause
+		// loops infinitely if there are no false clauses
+		int rand = (int) Math.random() * clauses.size();
+		if(clauseTF(clauses.get(rand), model) == -1)
+			return rand;
+		return findRandFalseClauseIndex(clauses, model);
 	}
 	
 	
 	public HashMap<Integer, Boolean> walkSAT(ArrayList<int []> clauses, long p, int maxFlips)
 	{
 		// randomize every symbol in model
+		HashMap<Integer, Boolean> model = new HashMap<Integer, Boolean>();
+		for(int [] clause : clauses)
+		{
+			for(int symbol : clause)
+			{
+				if(! model.containsKey(symbol))
+				{
+					double rand = Math.random();
+					Boolean value = Boolean.valueOf(false);
+					if(rand > .5)
+						value = Boolean.valueOf(true);
+					model.put(symbol, value);
+				}
+			}
+		}
 		
 		for(int i = 0; i < maxFlips; i++)
 		{
 			// if model is good then stop
-			int [] clause; // = random false clause in model
+			if(modelTF(clauses, model) == 1)
+				return model;
 			
-			// based on p do one of the following
-				// flip a random symbol in clause
-				// or flip the symbol that maximizes satisfied clauses in model after that flip
+			int [] clause = clauses.get(findRandFalseClauseIndex(clauses, model));
+			
+			if(Math.random() < p)
+			{
+				// flip a random symbol in the false clause
+				int key = clause[(int) Math.random() * clause.length];
+				Boolean val = model.get(key);
+				if(val.booleanValue())
+					model.replace(Integer.valueOf(key), Boolean.valueOf(false));
+				else
+					model.replace(Integer.valueOf(key), Boolean.valueOf(true));
+			}
+			else
+			{
+				// flip the symbol that maximizes satisfied clauses in model after that flip
+			}
 		}
 		
 		return null;
